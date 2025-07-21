939e6ca40cc85ad70ed99034644ee98d45de67cb
Gibran Fahad <gibranfahad1343787@gmail.com>
2025-07-20 21:02:36 -0500

    feat: modified the existing AWS infra with Azure infra

---
+++ terraform/modules/aks/variables.tf
@@ -0,0 +1,6 @@
+variable "aks_cluster_name" {}
+variable "resource_group_name" {}
+variable "location" {}
+variable "node_count" {}
+variable "vm_size" {}
+variable "kubernetes_version" {}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,19 +1,12 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-    random = {
-      source  = "hashicorp/random"
+    azurerm = {
+      source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
   }
-  required_version = ">= 1.0.0"
 }
 
-provider "aws" {
-  region = local.region
+provider "azurerm" {
+  features {}
 }
-
-provider "random" {} 
