85a4edb01e95e29a5b5300e6c422fb0cff71db57
Gibran Fahad <gibranfahad1343787@gmail.com>
2025-07-21 10:32:39 -0500

    feat: created module for vnet and subnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,13 @@ module "aks" {
   vm_size             = var.vm_size
   kubernetes_version  = var.kubernetes_version
 }
+
+# -------------------------------- NETWORK BLOCK -----------------------------------
+module "network" {
+  source              = "./modules/networks"
+  vnet_name           = var.vnet_name
+  vnet_address_space  = var.vnet_address_space
+  subnets             = var.subnets
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
---
+++ terraform/modules/networks/variables.tf
@@ -0,0 +1,9 @@
+variable "vnet_name" {}
+
+variable "vnet_address_space" {}
+
+variable "subnets" {}
+
+variable "location" {}
+
+variable "resource_group_name" {}
