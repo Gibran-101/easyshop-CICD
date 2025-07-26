458467f25b9d0e9f5fc748fcf02100f7519a0289
Gibran Fahad <gibranfahad1343787@gmail.com>
2025-07-25 20:38:17 -0500

    fix: updated acr_name in modules/acr/outputs.tf

--- terraform/modules/acr/outputs.tf
+++ terraform/modules/acr/outputs.tf
@@ -10,3 +10,7 @@ output "acr_admin_password" {
   value     = azurerm_container_registry.acr.admin_password
   sensitive = true
 }
+
+output "acr_name" {
+  value = azurerm_container_registry.acr.name
+}
